c3b2acbf374c9e781340ea0ba32dae529ae061be
loftwals <70249978+loftwals@users.noreply.github.com>
2022-06-16 02:46:13 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,10 @@
 const {Router} = require('express')
 const router = Router()
-
 const productos = []
-
-
-
 
 router.get('/productos', (req, res)=>{
     res.json(productos)
 })
-
 router.post('/productos', (req,res)=>{
     const{title, price, thumbnail} = req.body
     let id = productos.length
@@ -28,37 +23,27 @@ router.get('/productos/:id', (req, res)=>{
     }
     res.send(msj)
 })
-
 router.put('/productos/:id', (req,res)=>{
     let producto = productos.findIndex((prod) => { return prod.id == req.params.id});
     let msj;
     if (producto != -1){
         productos[producto]=req.body;
-        msj = {ok: "producto actualizado exitosamente"}
-        
+        msj = {ok: "producto actualizado"}
     } else {
-        
-        
-        msj = {error: "producto no encontrado"}
+        msj = {error: "no se encontro el producto"}
     }
     res.send(msj)
 })
-
 router.delete('/productos/:id', (req,res)=>{
     let producto = productos.findIndex((prod) => { return prod.id == req.params.id});
     let msj;
     console.log(producto);
     if (producto != -1){
         productos.splice(producto, 1);
-        msj = {ok: "producto eliminado exitosamente"}
-        
+        msj = {ok: "producto eliminado"}
     } else {
-        
         msj = {error: "producto no encontrado"}
     }
     res.send(msj)
 })
-
-
-
 module.exports = router
